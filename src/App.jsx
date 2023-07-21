4f4f35a01a3c52395814c84952353c3d9e0a74d8
ali mokhtari <alimokhtari7902@gmail.com>
2023-07-21 21:29:13 +0430

    add main header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
-  return <div className="wrapper bg-faded-dark"></div>;
+  return (
+    <div className="wrapper bg-faded-dark">
+      <Header />
+    </div>
+  );
 }
 
 export default App;

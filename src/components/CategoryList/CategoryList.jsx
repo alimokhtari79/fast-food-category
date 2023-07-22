d865f907b703756cb020ff31b0feab73463d67d5
ali mokhtari <alimokhtari7902@gmail.com>
2023-07-22 11:27:23 +0430

    loading components

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,33 +1,44 @@
 import { useEffect, useState } from 'react';
 import axios from '../../axios';
+import Loading from '../Loading/Loading';
 
 const CategoryList = () => {
   const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    axios
-      .get('/FoodCategory/categories')
-      .then((res) => setCategories(res.data));
+    const fetchCategories = async () => {
+      const response = await axios.get('/FoodCategory/categories');
+      setCategories(response.data);
+      setLoading(false);
+    };
+    fetchCategories();
   }, []);
+
   return (
     <nav className="container mt-n5">
       <div
         className="d-flex align-items-center bg-white rounded-3 shadow-lg py-4"
         style={{ height: '80px' }}
       >
-        <ul className="nav">
-          <li className="nav-item">
-            <a href="#" className="nav-link">
-              همه فست فودها
-            </a>
-          </li>
-          {categories.map((category) => (
-            <li className="nav-item" key={category.id}>
+        {loading ? (
+          <Loading />
+        ) : (
+          <ul className="nav">
+            <li className="nav-item">
               <a href="#" className="nav-link">
-                {category.name}
+                همه فست فودها
               </a>
             </li>
-          ))}
-        </ul>
+            {categories.map((category) => (
+              <li className="nav-item" key={category.id}>
+                <a href="#" className="nav-link">
+                  {category.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </nav>
   );

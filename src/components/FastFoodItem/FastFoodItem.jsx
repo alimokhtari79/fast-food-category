b148f26230bf6a2282a2cc3345c0d0f14c479b2d
ali mokhtari <alimokhtari7902@gmail.com>
2023-07-24 22:16:15 +0430

    add image placeholder

--- src/components/FastFoodItem/FastFoodItem.jsx
+++ src/components/FastFoodItem/FastFoodItem.jsx
@@ -1,4 +1,5 @@
 import { HiShoppingCart } from 'react-icons/hi';
+import './FastFoodItem.css';
 
 const FastFoodItem = ({ name, price, ingredients, imageUrl }) => {
   return (
@@ -6,7 +7,9 @@ const FastFoodItem = ({ name, price, ingredients, imageUrl }) => {
       <span className="badge badge-end badge-shadow bg-success fs-md fw-medium">
         قیمت : {price.toLocaleString()} تومان
       </span>
-      <img className="card-img-top" src={imageUrl} alt={name} />
+      <div className="card__placeholder">
+        <img className="card-img-top" src={imageUrl} />
+      </div>
       <div className="card-body text-center pt-3 pb-4 d-flex flex-column">
         <h5 className="mb-2">{name}</h5>
         <div className="fs-ms fw-bold text-muted mb-3">{ingredients}</div>
